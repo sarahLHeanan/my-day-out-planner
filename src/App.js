7ca350b47147206162fd5c5c7b7b2e98fbfe5923
SarahH <sarah.heanan@komododigital.co.uk>
2023-10-24 15:54:30 +0100

    bootstrap create react app and weather display component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import WeatherDisplay from "./components/WeatherDisplay/WeatherDisplay";
+
+
+function App() {
+  return (
+    <div>
+        <h1>My Day Out Planner</h1>
+        <WeatherDisplay></WeatherDisplay>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/WeatherDisplay/WeatherDisplay.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './WeatherDisplay.css';
+
+const WeatherDisplay = props => {
+    return (
+        <div className="weather-display">
+            <h2>The weather today is mostly</h2>
+            <p>Weather data here</p>
+            <p>We recommend an indoor/outdoor event</p>
+        </div>
+
+    )
+};
+
+export default WeatherDisplay;
