9939ee6ee0acb33041f305aed11dd870db5e7efc
SarahH <sarah.heanan@komododigital.co.uk>
2023-10-30 10:13:57 +0000

    Added example UI component
    With external as well as internal classes

--- src/components/Activity/AddActivity.js
+++ src/components/Activity/AddActivity.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import Card from '../UI/Card/Card.js';
+import classes from './activity.module.css';
+
 const AddActivity = () => {
 
     const addActivityHandler = (event) => {
@@ -7,54 +10,56 @@ const AddActivity = () => {
     };
 
     return (
-        <form onSubmit={addActivityHandler} className="bg-white mt-4">
-            <label htmlFor="name">Activity</label>
-            <input type="text" id="name" name="name"/>
+        <Card className={classes.input}>
+            <form onSubmit={addActivityHandler} className="mt-4">
+                <label htmlFor="name">Activity</label>
+                <input type="text" id="name" name="name"/>
 
-            {/*@todo turn this into location selector by postcode*/}
-            <label htmlFor="location">Area</label>
-            <select id="location" name="location">
-                <option value="sunderland">Sunderland</option>
-                <option value="newcastle">Newcastle</option>
-                <option value="durham">Durham</option>
-            </select>
-            <label htmlFor="ageRange">Children's Age Range</label>
-            <select id="ageRange" name="ageRange">
-                <option value="5">0-5</option>
-                <option value="10">5-12</option>
-                <option value="15">12-18</option>
-            </select>
-            <label>
-                <input
-                    type="radio"
-                    name="condition"
-                    value="indoor"
-                    checked={true}
-                    className="form-check-input"
-                />
-                Indoor
-            </label>
-            <label>
-                <input
-                    type="radio"
-                    name="condition"
-                    value="outdoor"
-                    checked={true}
-                    className="form-check-input"
-                />
-                Outdoor
-            </label>
-            <label htmlFor="price">Price Range</label>
-            <select id="price" name="price">
-                <option value="0">Free</option>
-                <option value="10">Up to £10</option>
-                <option value="20">Up to £20</option>
-                <option value="21">£20+</option>
-            </select>
-            <button type="submit">Find Activity</button>
-        </form>
+                {/*@todo turn this into location selector by postcode*/}
+                <label htmlFor="location">Area</label>
+                <select id="location" name="location">
+                    <option value="sunderland">Sunderland</option>
+                    <option value="newcastle">Newcastle</option>
+                    <option value="durham">Durham</option>
+                </select>
+                <label htmlFor="ageRange">Children's Age Range</label>
+                <select id="ageRange" name="ageRange">
+                    <option value="5">0-5</option>
+                    <option value="10">5-12</option>
+                    <option value="15">12-18</option>
+                </select>
+                <label>
+                    <input
+                        type="radio"
+                        name="condition"
+                        value="indoor"
+                        checked={true}
+                        className="form-check-input"
+                    />
+                    Indoor
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        name="condition"
+                        value="outdoor"
+                        checked={true}
+                        className="form-check-input"
+                    />
+                    Outdoor
+                </label>
+                <label htmlFor="price">Price Range</label>
+                <select id="price" name="price">
+                    <option value="0">Free</option>
+                    <option value="10">Up to £10</option>
+                    <option value="20">Up to £20</option>
+                    <option value="21">£20+</option>
+                </select>
+                <button type="submit">Find Activity</button>
+            </form>
+        </Card>
     )
-
 };
+
 
 export default AddActivity;
