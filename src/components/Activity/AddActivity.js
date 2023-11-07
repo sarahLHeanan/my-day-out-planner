0ac85e2e58ac4de4960ae4261da6d0dd7eca333b
SarahH <sarah.heanan@komododigital.co.uk>
2023-11-07 13:54:56 +0000

    set state in array instead of individual functions

--- src/components/Activity/AddActivity.js
+++ src/components/Activity/AddActivity.js
@@ -6,7 +6,6 @@ import classes from './activity.module.css';
 
 const AddActivity = (props) => {
 
-    // @todo return state in one array instead of slices
     //state variable array
     const [activities, setActivities] = useState([]);
     //individual field values
@@ -19,11 +18,6 @@ const AddActivity = (props) => {
 
     const [error, setError] = useState();
 
-    const addActivities = () => {
-
-    };
-
-
     const addActivityHandler = (event) => {
         event.preventDefault();
 
@@ -31,10 +25,16 @@ const AddActivity = (props) => {
             ...activities,
             {
                 name: enteredActivity,
+                location: enteredLocation,
+                ageRange: enteredAgeRange,
+                condition: enteredCondition,
+                price: enteredPrice,
+                childPrice: enteredChildPrice,
             },
         ]);
 
         console.log('activities set');
+        console.log(activities);
         //
         // //validation
         // if(
@@ -67,40 +67,17 @@ const AddActivity = (props) => {
         // console.log('adult price ' + enteredPrice);
         // console.log('child price ' + enteredChildPrice);
         //
-        // setActivity('');
-        // setLocation('');
-        // setAgeRange('');
-        // setCondition('');
-        // setPrice('');
-        // setChildPrice('');
-        // console.log('operation complete');
+        setActivity('');
+        setLocation('');
+        setAgeRange('');
+        setCondition('');
+        setPrice('');
+        setChildPrice('');
+        console.log('operation complete');
     };
 
-    const activityChangeHandler = (event) => {
-        setActivity(event.target.value);
-    }
-
-    const locationChangeHandler = (event) => {
-        setLocation(event.target.value);
-    }
 
     // @todo sort issue where select box value not recorded if its not 'changed' (set default null value?)
-    const ageRangeChangeHandler = (event) => {
-        setAgeRange(event.target.value);
-    }
-
-    const conditionChangeHandler = (event) => {
-        setCondition(event.target.value);
-    }
-
-    const priceChangeHandler = (event) => {
-        setPrice(event.target.value);
-    }
-
-    const childPriceChangeHandler = (event) => {
-        setChildPrice(event.target.value);
-    }
-
     const errorHandler = () => {
         setError(null);
     }
@@ -121,47 +98,57 @@ const AddActivity = (props) => {
                            value={enteredActivity}
                            onChange={(e) => setActivity(e.target.value)}
                     />
-                    {/*<input type="text" id="name" name="name" value={enteredActivity} onChange={activityChangeHandler}/>*/}
-
                     {/*@todo turn this into location selector by postcode*/}
-                    {/*<label htmlFor="location">Area</label>*/}
-                    {/*<select id="location" name="location" value={enteredLocation} onChange={locationChangeHandler}>*/}
-                    {/*    <option value="sunderland">Sunderland</option>*/}
-                    {/*    <option value="newcastle">Newcastle</option>*/}
-                    {/*    <option value="durham">Durham</option>*/}
-                    {/*</select>*/}
-                    {/*<label htmlFor="ageRange">Children's Age Range</label>*/}
-                    {/*<select id="ageRange" name="ageRange" value={enteredAgeRange} onChange={ageRangeChangeHandler}>*/}
-                    {/*    <option value="5">0-5</option>*/}
-                    {/*    <option value="10">5-12</option>*/}
-                    {/*    <option value="15">12-18</option>*/}
-                    {/*</select>*/}
-                    {/*<label>*/}
-                    {/*    <input*/}
-                    {/*        type="radio"*/}
-                    {/*        name="condition"*/}
-                    {/*        value="indoor"*/}
-                    {/*        checked={true}*/}
-                    {/*        className="form-check-input"*/}
-                    {/*        onChange={conditionChangeHandler}*/}
-                    {/*    />*/}
-                    {/*    Indoor*/}
-                    {/*</label>*/}
-                    {/*<label>*/}
-                    {/*    <input*/}
-                    {/*        type="radio"*/}
-                    {/*        name="condition"*/}
-                    {/*        value="outdoor"*/}
-                    {/*        className="form-check-input"*/}
-                    {/*        onChange={conditionChangeHandler}*/}
-                    {/*    />*/}
-                    {/*    Outdoor*/}
-                    {/*</label>*/}
-                    {/*<label htmlFor="price">Price per adult</label>*/}
-                    {/*<input type="number" id="price" name="price" value={enteredPrice} onChange={priceChangeHandler}/>*/}
-                    {/*<label htmlFor="price">Price per child</label>*/}
-                    {/*<input type="number" id="childPrice" name="child-price" value={enteredChildPrice} onChange={childPriceChangeHandler}/>*/}
-                    {/*<button onClick={() => addActivities()}>Add Activity</button>*/}
+                    <label htmlFor="location">Area</label>
+                    <select id="location"
+                            name="location"
+                            value={enteredLocation}
+                            onChange={(e) => setLocation(e.target.value)}>
+                        <option value="sunderland">Sunderland</option>
+                        <option value="newcastle">Newcastle</option>
+                        <option value="durham">Durham</option>
+                    </select>
+                    <label htmlFor="ageRange">Children's Age Range</label>
+                    <select id="ageRange"
+                            name="ageRange"
+                            value={enteredAgeRange}
+                            onChange={(e) => setAgeRange(e.target.value)}>
+                        <option value="5">0-5</option>
+                        <option value="10">5-12</option>
+                        <option value="15">12-18</option>
+                    </select>
+                    <label>
+                        <input
+                            type="radio"
+                            name="condition"
+                            value="indoor"
+                            checked={true}
+                            className="form-check-input"
+                            onChange={(e) => setCondition(e.target.value)}
+                        />
+                        Indoor
+                    </label>
+                    <label>
+                        <input
+                            type="radio"
+                            name="condition"
+                            value="outdoor"
+                            className="form-check-input"
+                            onChange={(e) => setCondition(e.target.value)}
+                        />
+                        Outdoor
+                    </label>
+                    <label htmlFor="price">Price per adult</label>
+                    <input type="number"
+                           id="price" name="price"
+                           value={enteredPrice}
+                           onChange={(e) => setPrice(e.target.value)}/>
+                    <label htmlFor="child-price">Price per child</label>
+                    <input type="number"
+                           id="childPrice"
+                           name="child-price"
+                           value={enteredChildPrice}
+                           onChange={(e) => setChildPrice(e.target.value)}/>
                     <button type="submit">Add Activity</button>
                 </form>
             </Card>
